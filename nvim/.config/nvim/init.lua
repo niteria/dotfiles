374b5afb52b949e635c14a09f0105dc6b65c202e
niteria <niteria@gmail.com>
2023-07-11 21:00:16 +0000

    Migrate to lazy.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,85 +15,99 @@ if vim.fn.filereadable(python2_path) == 1 then
   vim.g.python2_host_prog = vim.fn.expand(python2_path)
 end
 
-local Plug = vim.fn["plug#"]
+-- leader and localleader
+vim.g.mapleader = ","
+vim.g.maplocalleader = "\\"
 
-vim.fn["plug#begin"]("~/.config/nvim/plugged")
--- do :PlugInstall to install the plugins
--- inkpot colorscheme
-Plug("ciaranm/inkpot")
--- sensible defaults
-Plug("tpope/vim-sensible")
--- common dependencies
-Plug("nvim-lua/popup.nvim")
-Plug("nvim-lua/plenary.nvim")
--- complete command-line (: / etc.) from the current file
-Plug("vim-scripts/CmdlineComplete")
--- :Ack command
-Plug("wincent/ferret")
--- Bracketed paste
-Plug("wincent/terminus")
--- Awesome git integration, :GDiff, :GBlame, etc...
-Plug("tpope/vim-fugitive")
--- File browser
-Plug("justinmk/vim-dirvish")
--- Show git status in the file browser
-Plug("kristijanhusak/vim-dirvish-git")
--- Mercurial integration
-Plug("ludovicchabant/vim-lawrencium")
--- - goes up a directory
-Plug("tpope/vim-vinegar")
--- Haskell enhancements
-Plug("parsonsmatt/vim2hs")
--- Undo tree
-Plug("mbbill/undotree")
--- Coerce: crm - CoeRce Mixed-case, crs - CoeRce Snake-case
-Plug("tpope/tpope-vim-abolish")
--- Autocompletion framework
-Plug("Shougo/deoplete.nvim", {
-  ["do"] = ":UpdateRemotePlugins",
-})
--- Autocompletion from tmux buffers, integrates with deoplete
-Plug("wellle/tmux-complete.vim")
--- [- : Move to previous line of lesser indent than the current line.
-Plug("jeetsukumaran/vim-indentwise")
--- Make QuickFix window do what I want
-Plug("yssl/QFEnter")
--- Ghcid - a tiny Haskell IDE
-Plug("ndmitchell/ghcid", { rtp = "plugins/nvim" })
--- Typescript support
-Plug("leafgarland/typescript-vim")
-Plug("peitalin/vim-jsx-typescript")
--- Projectionist for .c .cpp .cc <-> .h file switching
-Plug("tpope/vim-projectionist")
--- Display Github url of the current file with :GitHubURL
-Plug("pgr0ss/vim-github-url")
--- Multi-entry selection UI.
-Plug("junegunn/fzf")
--- Supports syntax highlighting
-Plug("vim-syntastic/syntastic")
--- Syntax highlighting for Swift
-Plug("keith/swift.vim")
--- Code autocompletion for Swift
-Plug("keith/sourcekittendaemon.vim")
--- Nix syntax highlighting
-Plug("LnL7/vim-nix")
--- Fancy Deep Learning code suggestions
-if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-  Plug("tbodt/deoplete-tabnine", { ["do"] = "powershell.exe .\\install.ps1" })
-else
-  Plug("tbodt/deoplete-tabnine", { ["do"] = "./install.sh" })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-Plug("niteria/neomake-platformio")
-Plug("embear/vim-localvimrc")
-Plug("neoclide/coc.nvim", { branch = "release" })
-Plug("neovimhaskell/haskell-vim")
-Plug("vim-autoformat/vim-autoformat")
--- vim lua autocompletion
-Plug("rafcamlet/coc-nvim-lua")
-Plug("akinsho/toggleterm.nvim", { tag = "*" })
-Plug("lewis6991/gitsigns.nvim", { tag = "*" })
-Plug("nvim-telescope/telescope.nvim", { tag = "0.1.2" })
-vim.fn["plug#end"]()
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- inkpot colorscheme
+  "ciaranm/inkpot",
+  -- sensible defaults
+  "tpope/vim-sensible",
+  -- common dependencies
+  "nvim-lua/popup.nvim",
+  "nvim-lua/plenary.nvim",
+  -- complete command-line (: / etc., from the current file
+  "vim-scripts/CmdlineComplete",
+  -- :Ack command
+  "wincent/ferret",
+  -- Bracketed paste
+  "wincent/terminus",
+  -- Awesome git integration, :GDiff, :GBlame, etc...
+  "tpope/vim-fugitive",
+  -- File browser
+  "justinmk/vim-dirvish",
+  -- Show git status in the file browser
+  "kristijanhusak/vim-dirvish-git",
+  -- Mercurial integration
+  "ludovicchabant/vim-lawrencium",
+  -- - goes up a directory
+  "tpope/vim-vinegar",
+  -- Haskell enhancements
+  "parsonsmatt/vim2hs",
+  -- Undo tree
+  "mbbill/undotree",
+  -- Coerce: crm - CoeRce Mixed-case, crs - CoeRce Snake-case
+  "tpope/tpope-vim-abolish",
+  -- Autocompletion framework
+  { "Shougo/deoplete.nvim", build = ":UpdateRemotePlugins" },
+  -- Autocompletion from tmux buffers, integrates with deoplete
+  "wellle/tmux-complete.vim",
+  -- [- : Move to previous line of lesser indent than the current line.
+  "jeetsukumaran/vim-indentwise",
+  -- Make QuickFix window do what I want
+  "yssl/QFEnter",
+  -- Ghcid - a tiny Haskell IDE
+  {
+    "ndmitchell/ghcid",
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/plugins/nvim")
+    end,
+  },
+  -- Typescript support
+  "leafgarland/typescript-vim",
+  "peitalin/vim-jsx-typescript",
+  -- Projectionist for .c .cpp .cc <-> .h file switching
+  "tpope/vim-projectionist",
+  -- Display Github url of the current file with :GitHubURL
+  "pgr0ss/vim-github-url",
+  -- Multi-entry selection UI.
+  "junegunn/fzf",
+  -- Supports syntax highlighting
+  "vim-syntastic/syntastic",
+  -- Syntax highlighting for Swift
+  "keith/swift.vim",
+  -- Code autocompletion for Swift
+  "keith/sourcekittendaemon.vim",
+  -- Nix syntax highlighting
+  "LnL7/vim-nix",
+  "niteria/neomake-platformio",
+  "embear/vim-localvimrc",
+  { "neoclide/coc.nvim", branch = "release" },
+  "neovimhaskell/haskell-vim",
+  "vim-autoformat/vim-autoformat",
+  -- vim lua autocompletion
+  "rafcamlet/coc-nvim-lua",
+  { "akinsho/toggleterm.nvim", tag = "v2.7.0" },
+  { "lewis6991/gitsigns.nvim", tag = "v0.6" },
+  { "nvim-telescope/telescope.nvim", tag = "0.1.2" },
+  -- "folke/which-key.nvim",
+  -- { "folke/neoconf.nvim", cmd = "Neoconf" },
+  -- "folke/neodev.nvim",
+})
 
 require("user.gitsigns")
 require("user.telescope")
@@ -104,10 +118,6 @@ require("user.tmux-buffers")
 require("user.autoformat")
 -- use inkpot colorscheme
 vim.cmd("colorscheme inkpot")
-
--- leader and localleader
-vim.g.mapleader = ","
-vim.g.maplocalleader = "\\"
 
 -- move between tabs with Tab and Shift-Tab
 vim.keymap.set("n", "<Tab>", function()
